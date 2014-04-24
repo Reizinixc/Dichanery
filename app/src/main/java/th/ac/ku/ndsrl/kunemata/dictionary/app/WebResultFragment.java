14c1a31399c214cf9a5a1bc59808c12f3c7c3e92
Kune Keiseiie <keiseiie_kune@hotmail.co.jp>
2014-04-24 09:23:11 +0700

    Added pull to refresh for refreshing search result

--- app/src/main/java/th/ac/ku/ndsrl/kunemata/dictionary/app/WebResultFragment.java
+++ app/src/main/java/th/ac/ku/ndsrl/kunemata/dictionary/app/WebResultFragment.java
@@ -1,15 +1,20 @@
 package th.ac.ku.ndsrl.kunemata.dictionary.app;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
+import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
 
 public class WebResultFragment extends Fragment {
    private static final String WEBRESULT_API_URL = "http://dict.longdo.com/mobile.php?search=%s&nocache=1";
 
+   private PullToRefreshLayout pullToRefreshLayout;
    private WebView wevResult;
 
    @Override
@@ -18,6 +23,24 @@ public class WebResultFragment extends Fragment {
       wevResult = (WebView) view.findViewById(R.id.wevResult);
 
       return view;
+   }
+
+   @Override
+   public void onViewCreated(View view, Bundle savedInstanceState) {
+      final Activity activity = getActivity();
+      pullToRefreshLayout = (PullToRefreshLayout) view.findViewById(R.id.ptr_layout);
+      ActionBarPullToRefresh.from(activity)
+            .allChildrenArePullable()
+            .listener(new OnRefreshListener() {
+
+               @Override
+               public void onRefreshStarted(View view) {
+                  showResult(activity.getIntent().getDataString());
+                  pullToRefreshLayout.setRefreshComplete();
+               }
+
+            })
+            .setup(pullToRefreshLayout);
    }
 
    public void showResult(String word) {
